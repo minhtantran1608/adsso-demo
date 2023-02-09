ed66cc1169b3a4c61c91b76dda904a350fe20ba7
Mã Chấn Phong <75919476+redwindfuu@users.noreply.github.com>
2023-02-09 09:52:12 +0700

    push server v7

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,11 +3,25 @@ const fetch = require("../utils/fetch");
 const config = require("../config");
 const router = require("express").Router();
 
-router.get("/", isAuthentication, async function (req, res) {
+router.get("/me", isAuthentication, async function (req, res) {
   try {
     console.log(req?.user?.accessToken);
     const graphResponse = await fetch(
       `${config.graph_api_endpoint}/v1.0/me`,
+      req.user.accessToken
+    );
+    log
+    res.json(graphResponse);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/", isAuthentication, async function (req, res) {
+  try {
+    console.log(req?.user?.accessToken);
+    const graphResponse = await fetch(
+      `${config.graph_api_endpoint}/v1.0/users`,
       req.user.accessToken
     );
     res.json(graphResponse);
